d7254588572c35f8c707d8d8e2fdc4608f776f13
Clare Matthews <cmatthew@thoughtworks.com>
2015-04-20 13:54:46 +0100

    strip html tags; use csoup

--- project.clj
+++ project.clj
@@ -34,6 +34,7 @@
                  [org.pegdown/pegdown "1.2.0"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [hickory "0.5.4"]
+                 [be.niwi/cljsoup "0.1.0"]
                  [clj-soup/clojure-soup "0.1.2"]
                  [diff-match-patch-clj "1.0.0-SNAPSHOT"]
                  [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "r239" :exclusions [com.google.guava/guava com.google.code.findbugs/jsr305]]]
--- src/objective8/diff.clj
+++ src/objective8/diff.clj
@@ -1,6 +1,8 @@
 (ns objective8.diff
   (:require [diff-match-patch-clj.core :as dmp]
             [net.cgrand.enlive-html :as html]
+            [cljsoup.core :as soup-core]
+            [cljsoup.nodes :as soup-nodes]
             [jsoup.soup :as jsoup]
             [objective8.utils :as utils]
             [objective8.drafts :as drafts]))
@@ -21,18 +23,22 @@
 
 (defn remove-tags [hiccup-draft]
   (let [html-draft (utils/hiccup->html hiccup-draft)
-        parsed-draft (jsoup/parse html-draft)
-        body (jsoup/select [:body] parsed-draft)]
-    (prn html-draft)
+        parsed-draft (soup-core/from-string html-draft)
+        body (soup-nodes/body parsed-draft)]
     (prn parsed-draft)
-    (prn (jsoup/text body))))
+    (prn "---------------------------------")
+    (prn body)
+    (prn "***********************")
+    (prn (soup-nodes/select parsed-draft "body > p"))
+    (prn "$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+    (prn (html/text (soup-nodes/select parsed-draft "body > p")))))
 
 (defn get-hiccup-draft [draft-id] 
   (apply list (:content (drafts/retrieve-draft draft-id))))
 
 (defn get-all-views []
-  (let [draft-1 (get-hiccup-draft 30868)
-        draft-2 (get-hiccup-draft 30869)
+  (let [draft-1 (get-hiccup-draft 31572)
+        draft-2 (get-hiccup-draft 31573)
         diffs (dmp/cleanup! (dmp/diff (get-text draft-1) (get-text draft-2)))]
     (remove-tags draft-1)
     {:first-draft draft-1
@@ -42,8 +48,8 @@
 
 
 (defn get-diff []
-  (let [draft-1 (get-hiccup-draft 30868)
-        draft-2 (get-hiccup-draft 30869)
+  (let [draft-1 (get-hiccup-draft 31572)
+        draft-2 (get-hiccup-draft 31573)
         diffs (dmp/cleanup! (dmp/diff (get-text draft-1) (get-text draft-2)))
         inserts (remove nil? (map get-insert diffs))
         deletes (remove nil? (map get-delete diffs))]
